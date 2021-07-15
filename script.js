e79da9fb9a92de8984465a3a9fa77d9ac7151574
freestyletear <freestyletear@gmail.com>
2021-07-15 17:05:40 +0300

    add code

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function akanNameCalculator() {
   } catch {
     document.getElementById("emptyDate").innerHTML = "*Date field required";
   }
-    //Day
+  //Day
 
   let date = new Date(inputDate);
   let birthDate = date.getDay();
@@ -29,18 +29,14 @@ function akanNameCalculator() {
   ];
   let femaleAkan = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-   // Find Male Akan from BirthDate
+  // Find Male Akan from BirthDate
 
   if (document.getElementById("genderMale").checked && inputDate != "") {
     document.getElementById("results").innerHTML =
-      "Your Akan name is: " +
-      "<strong>" +
-      maleAkan[birthDate] +
-      "</strong>";
+      "Your Akan name is: " + "<strong>" + maleAkan[birthDate] + "</strong>";
   }
 
   // Find Female Akan from birthDate
-
   else if (document.getElementById("genderFemale").checked && inputDate != "") {
     document.getElementById("results").innerHTML =
       "According to your gender, your Akan name is: " +
@@ -48,3 +44,16 @@ function akanNameCalculator() {
       femaleAkan[birthDate] +
       "</strong>";
   }
+  // Warning
+  else if (
+    !document.getElementById("genderFemale").checked &&
+    !document.getElementById("genderMale").checked
+  ) {
+    document.getElementById("not-checked").innerHTML = "*Select your gender";
+  }
+  // Background image
+
+  setInterval(function () {
+    var bg;
+  });
+}
